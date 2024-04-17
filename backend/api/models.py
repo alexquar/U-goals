8412713989209a1b8007ff9ce11b783cfb73da1f
alexquar <alexanderquarrie@gmail.com>
2024-04-16 22:53:57 -0400

    goal model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Goal(models.Model):
+    title = models.CharField(max_length=100)
+    about = models.TextField()
+    complete_by = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="goals") #foreign key that references the user that ones it, can do the same later to attach plans onto goals
+
+    def __str__(self):
+        return self.title
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,13 +1,20 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from .models import Goal
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id','username','password')
-        extra_kwargs = {"password":{"write_only":True}}
+        extra_kwargs = {"password":{"write_only":True}} #sets the password prop to be write only
     
     def create(self,validated_data):
         user=User.objects.create_user(**validated_data)
         return user
+    
+
+class GoalsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Goal
+        fields = ('title','about','complete_by','created_at', 'author')
+        extra_kwargs = {"author":{"read_only":True}} #sets the author prop to be read only
