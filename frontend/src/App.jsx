bdf6f197284ce19cf15805a28e4a16db578d88d4
alexquar <alexanderquarrie@gmail.com>
2024-04-17 23:22:19 -0400

    router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,39 @@
-import './styles/App.css'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import ProtectedRoute from './components/ProtectedRoute';
 function App() {
-
+  function Logout() {
+    localStorage.clear()
+    return <Navigate to="/login" />
+  }
+  
+  function RegisterAndLogout() {
+    localStorage.clear()
+    return <Signup />
+  }
   return (
     <>
+      <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/signup" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
